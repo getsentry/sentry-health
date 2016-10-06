c811e82d20b489f388243cd4038b991d5ecd2704
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-06 21:47:09 +0200

    Import everything on package import

---
+++ tervis/__init__.py
@@ -0,0 +1,8 @@
+def __init():
+    """Make sure we import everything to avoid stupid issues later on."""
+    from .utils import iter_modules
+    for module in iter_modules(__name__):
+        __import__(module)
+
+__init()
+del __init
--- tervis/utils.py
+++ tervis/utils.py
@@ -1,3 +1,6 @@
+import sys
+import pkgutil
+
 from ._compat import iteritems
 
 
@@ -29,3 +32,18 @@ def iter_segments(items):
             if x.isdigit():
                 x = int(x)
             yield x
+
+
+def iter_modules(path):
+    __import__(path)
+    module = sys.modules[path]
+    path = getattr(module, '__path__', None)
+    if path is None:
+        raise ValueError('%r is not a package' % import_path)
+    basename = module.__name__ + '.'
+    for importer, modname, ispkg in pkgutil.iter_modules(path):
+        modname = basename + modname
+        yield modname
+        if ispkg:
+            for item in iter_modules(modname, include_packages, True):
+                yield item
