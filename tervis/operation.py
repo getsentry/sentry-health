aca61e2a7781160085fabe89fb17eca50e43370e
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-26 16:17:30 +0200

    Added operator

---
+++ tervis/operation.py
@@ -0,0 +1,19 @@
+from .depmgr import DependencyMount, DependencyDescriptor
+from .environment import CurrentEnvironment
+
+
+class CurrentOperation(DependencyDescriptor):
+    pass
+
+
+class Operation(DependencyMount):
+    env = CurrentEnvironment()
+
+    def __init__(self, env, req=None, project=None):
+        DependencyMount.__init__(self,
+            parent=env,
+            scope='operation',
+            descriptor_type=CurrentOperation
+        )
+        self.req = req
+        self.project = project
