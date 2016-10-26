b236e6b2fa5e46a0724637ac4f29b67b50414cee
Armin Ronacher <armin.ronacher@active-4.com>
2016-10-26 16:17:25 +0200

    Update apiserver

--- tervis/apiserver.py
+++ tervis/apiserver.py
@@ -5,9 +5,11 @@ from ipaddress import ip_address
 
 from aiohttp import web
 
-from .auth import AuthManager
+from .auth import Auth
 from .producer import Producer
 from .exceptions import ApiError, PayloadTooLarge, ClientReadFailed
+from .operation import CurrentOperation, Operation
+from .depmgr import DependencyDescriptor, DependencyMount
 from ._compat import scalar_types, number_types, string_types
 
 
@@ -53,24 +55,35 @@ def normalize_event(event):
     }
 
 
-class Server(object):
+class CurrentEndpoint(DependencyDescriptor):
+    pass
 
-    def __init__(self, env):
-        self.env = env
-        self.auth_manager = AuthManager(env)
-        self.producer = Producer(env)
-        self.app = web.Application()
-        self.app.router.add_route(
-            method='POST',
-            path='/events/{project}',
-            handler=self.on_submit_event,
-            name='submit_event'
+
+class Endpoint(DependencyMount):
+    op = CurrentOperation()
+
+    def __init__(self, op):
+        DependencyMount.__init__(self,
+            parent=op,
+            descriptor_type=CurrentEndpoint
         )
 
-        self.max_json_packet = env.get_config('apiserver.limits.max_json_packet')
+    @classmethod
+    async def full_dispatch(cls, env, req):
+        project = req.match_info.get('project')
+        async with Operation(env, req, project) as op:
+            async with cls(op) as self:
+                return await self.handle()
 
-    async def accept_event(self, request):
-        line = await request.content.readline()
+    async def handle(self):
+        raise NotImplementedError('This endpoint cannot handle')
+
+
+class SubmitEventEndpoint(Endpoint):
+    auth = Auth()
+
+    async def accept_event(self):
+        line = await self.op.req.content.readline()
         if not line:
             return
         try:
@@ -81,22 +94,20 @@ class Server(object):
         except IOError as e:
             raise ClientReadFailed(str(e))
 
-    async def process_event(self, event, auth):
+    async def process_event(self, event):
         with self.producer.partial_guard():
-            self.producer.produce_event(auth.project, event,
-                                        auth.timestamp)
+            self.producer.produce_event(self.auth.project, event,
+                                        self.auth.timestamp)
 
-    async def on_submit_event(self, request):
-        project = request.match_info['project']
-        auth = await self.auth_manager.auth_from_request(request, project)
+    async def handle(self):
         errors = []
         events = 0
         while 1:
             try:
-                event = await self.accept_event(request)
+                event = await self.accept_event()
                 if event is None:
                     break
-                await self.process_event(event, auth)
+                await self.process_event(event)
                 events += 1
             except ApiError as e:
                 errors.append(e.to_json())
@@ -105,6 +116,24 @@ class Server(object):
             'errors': errors,
             'events': events,
         }))
+
+
+class Server(object):
+
+    def __init__(self, env):
+        self.env = env
+        self.auth_manager = AuthManager(env)
+        self.producer = Producer(env)
+        self.app = web.Application()
+
+        self.app.router.add_route(
+            method='POST',
+            path='/events/{project}',
+            handler=SubmitEventEndpoint.full_dispatch,
+            name='submit_event'
+        )
+
+        self.max_json_packet = env.get_config('apiserver.limits.max_json_packet')
 
     def run(self, host=None, port=None):
         if host is None:
